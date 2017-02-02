e7840bc22914f699c926d6689e258ec1bd50df2c
martinrun <martinrun@gmail.com>
2017-02-02 20:26:02 +0100

    Update 2.3chessBoard.js
    Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board. When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.

--- 2.3chessBoard.js
+++ 2.3chessBoard.js
@@ -1,5 +1,3 @@
-/* Assignment: Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board. When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.*/
-
 var size=8
 var result=""
 for(var i=0;i<size;i++) {         //outer loop creates rows thanks to "\n"
